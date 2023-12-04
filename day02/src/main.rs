7219785836ab9e235a7a9bb8332af537e5998e65
jan <jan.pruijs@student.hu.nl>
2023-12-04 02:09:24 +0100

    progress

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,30 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_lines("./input/input.txt");
+
+    let result: Vec<_> = input
+        .iter()
+        .map(|line| line.split_once(": ").unwrap())
+        .map(|(game_part , color_part)| (&game_part[5..], color_part))
+
+
+        .collect();
+
+    println!("{:?}", result)
 }
+
+struct Game {
+    id: u32,
+    red: u32,
+    blue: u32,
+    green: u32,
+}
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap()  // panic on possible file-reading errors
+        .lines()  // split the string into an iterator of string slices
+        .map(String::from)  // make each slice into a string
+        .collect()  // gather them together into a vector
+}
+
